efdd039f5e0d1ef5835673ce129ac66eab9ae2a4
seif basel <seifbasel950@gmail.com>
2025-04-29 11:47:15 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={`${rubik.variable} ${rubikItalic.variable} bg-background antialiased`}>
+      <body className={`${rubik.variable} ${rubikItalic.variable}  antialiased`}>
         {children}
       </body>
     </html>
