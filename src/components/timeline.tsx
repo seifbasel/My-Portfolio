67b242cf7b951cd004d992069cff6b1157ec543e
seif basel <seifbasel950@gmail.com>
2025-04-29 14:16:24 +0300

    fix

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -61,14 +61,14 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-background dark:subtitle to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute md:left-8 left-8 top-0 bg-text overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-background dark:subtitle to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
               height: heightTransform,
               opacity: opacityTransform,
             }}
-            className="absolute inset-x-0 top-0 w-[10px] bg-gradient-to-t from-primary via-purple-500 to-transparent from-[0%] via-[10%] rounded-full"
+            className="absolute inset-x-0 top-0 w-[10px] bg-gradient-to-t  from-primary via-purple-500 to-transparent from-[0%] via-[10%] rounded-full"
           />
         </div>
       </div>
@@ -89,19 +89,19 @@ const TimelineItem = ({ item, isEven }: { item: TimelineEntry; isEven: boolean }
     <div ref={ref} className="flex justify-start pt-10 pb-10 md:pt-40 md:gap-10">
       <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
         <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-text flex items-center justify-center">
-          <div className="h-4 w-4 rounded-full bg-background border border-text p-2" />
+          <div className="h-4 w-4 rounded-full  bg-background border border-text p-2" />
         </div>
         <motion.h3 
           initial={{ opacity: 0, x: -20 }}
           animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -20 }}
           transition={{ duration: .5, delay: 0.5 }}
-          className="hidden md:block text-xl md:pl-20 md:text-5xl  font-rubik text-text"
+          className="hidden md:block text-xl md:pl-20 md:text-5xl   font-rubik text-text"
         >
           {item.title}
         </motion.h3>
       </div>
 
-      <div className="relative pl-16 md:pr-4 md:pl-4 w-full">
+      <div className="relative pl-16 md:pr-4 md:pl-4 w-full bg">
         <motion.div
           ref={contentRef}
           initial={{ opacity: 0, y: 30 }}
